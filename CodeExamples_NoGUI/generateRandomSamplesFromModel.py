d6381d3d5018ad9dec95b449a488b1aa15233688
damien <damien.dasnoy@gmail.com>
2022-03-20 20:08:35 +0100

    add a function to generate random deformations in the deformable data augmentation toolbox

--- CodeExamples_NoGUI/generateRandomSamplesFromModel.py
+++ CodeExamples_NoGUI/generateRandomSamplesFromModel.py
@@ -1,38 +1,39 @@
 from Core.IO.serializedObjectIO import saveSerializedObjects, loadDataStructure
-from Core.Processing.DeformableDataAugmentationToolBox.generateRandomSamplesFromModel import generateRandomSamplesFromModel
+from Core.Processing.DeformableDataAugmentationToolBox.generateRandomSamplesFromModel import generateRandomImagesFromModel, generateRandomDeformationsFromModel
 import os
 from pathlib import Path
 import cProfile
 import time
 import numpy as np
 import concurrent
+import matplotlib.pyplot as plt
 
 testDataPath = os.path.join(Path(os.getcwd()).parent.absolute(), 'testData/')
 
 ## read a serialized dynamic sequence
-# dataPath = testDataPath + "superLightDynSeqWithMod.p"
-dataPath = '/home/damien/Desktop/Patient0/Patient0_Model_bodyCropped.p'
-dynMod = loadDataStructure(dataPath)[0]
+dataPath = testDataPath + "superLightDynSeqWithMod.p"
+# dataPath = '/home/damien/Desktop/Patient0/Patient0_Model_bodyCropped.p'
+patient = loadDataStructure(dataPath)[0]
+dynMod = patient.getPatientDataOfType("Dynamic3DModel")[0]
 
-pr = cProfile.Profile()
-pr.enable()
+
+imageList = []
 
 startTime = time.time()
-imageList = generateRandomSamplesFromModel(dynMod, numberOfSamples=5, ampDistribution='gaussian', tryGPU=True)
-print('first test done in ', np.round(time.time()- startTime, 2))
 
-pr.disable()
-pr.print_stats(sort="cumulative")
-#
-# imageList = []
-#
-# test = [0, 0, 0, 0, 0]
-# # processes = []
-# # for deformationIndex, deformation in enumerate(deformationList):
-# print('start multi process deformation')
-# startTime = time.time()
-# with concurrent.futures.ProcessPoolExecutor() as executor:
-#
-#     results = [executor.submit(generateRandomSamplesFromModel, dynMod, tryGPU=False) for _ in range(5)]
-#     # imageList += results
-# print('second test done in ', np.round(time.time()- startTime, 2))
+defList = generateRandomDeformationsFromModel(dynMod, numberOfSamples=1, ampDistribution='gaussian')
+im1 = defList[0].deformImage(dynMod.midp, fillValue='closest', tryGPU=True)
+
+print('first test done in ', np.round(time.time() - startTime, 2))
+
+plt.figure()
+plt.imshow(im1.imageArray[:, 50, :])
+plt.show()
+
+startTime = time.time()
+imageList = generateRandomImagesFromModel(dynMod, numberOfSamples=1, ampDistribution='gaussian', tryGPU=True)
+print('second test done in ', np.round(time.time() - startTime, 2))
+
+plt.figure()
+plt.imshow(imageList[0].imageArray[:, 50, :])
+plt.show()
