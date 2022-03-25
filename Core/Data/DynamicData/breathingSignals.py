e58da9dc0fdf96c5e88e0b673dbf2f1c034a9008
Gauthier Rotsart de Hertaing <grotsartdehe@maxwell.local>
2022-03-25 16:14:28 +0100

    changes in breathing signal generation

--- Core/Data/DynamicData/breathingSignals.py
+++ Core/Data/DynamicData/breathingSignals.py
@@ -19,8 +19,8 @@ class BreathingSignal(PatientData):
 
 # synthetic breathing data
 class SyntheticBreathingSignal(BreathingSignal):
-    def __init__(self, amplitude=10, variationAmplitude=5, breathingPeriod=4, variationFrequency=0.5, shift=5, mean=0,
-                 variance=0.1, samplingPeriod=0.2, simulationTime=100, meanEvent=2 / 30, patientInfo=None, name="Breathing Signal"):
+    def __init__(self, amplitude=10, variationAmplitude=5, breathingPeriod=4, variationFrequency=0.5, shift=5, meanNoise=0,
+                 varianceNoise=0.1, samplingPeriod=0.2, simulationTime=100, meanEvent=2 / 30, patientInfo=None, name="Breathing Signal"):
         super().__init__(patientInfo=patientInfo, name=name)
 
         self.amplitude = amplitude  # amplitude (mm)
@@ -28,8 +28,8 @@ class SyntheticBreathingSignal(BreathingSignal):
         self.breathingPeriod = breathingPeriod  # periode respiratoire (s)
         self.variationFrequency = variationFrequency  # variation de frequence possible (Hz)
         self.shift = shift  # shift du signal (mm)
-        self.mean = mean
-        self.variance = variance
+        self.meanNoise = meanNoise
+        self.varianceNoise = varianceNoise
         self.samplingPeriod = samplingPeriod  # periode d echantillonnage
         self.simulationTime = simulationTime  # temps de simulation
         self.meanEvent = meanEvent  # moyenne des evenements aleatoires
@@ -37,6 +37,6 @@ class SyntheticBreathingSignal(BreathingSignal):
 
     def generateBreathingSignal(self):
         self.timestamps, self.breathingSignal = signalGeneration(self.amplitude, self.variationAmplitude, self.breathingPeriod,
-                                                                 self.variationFrequency, self.shift, self.mean, self.variance,
+                                                                 self.variationFrequency, self.shift, self.meanNoise, self.varianceNoise,
                                                                  self.samplingPeriod, self.simulationTime, self.meanEvent)
         return self.breathingSignal
