ff29133cc2178f13e4c2fd1ff3dfdf8f5f3c43bd
Gauthier Rotsart de Hertaing <grotsartdehe@maxwell.local>
2022-03-28 13:13:25 +0200

    breathing signal generation v1

--- Core/Processing/DeformableDataAugmentationToolBox/BreathingSignalGeneration.py
+++ Core/Processing/DeformableDataAugmentationToolBox/BreathingSignalGeneration.py
@@ -33,9 +33,7 @@ def vectorSimulation(L,diff,mean,sigma,Tend,t,timestamp):
             y[(t>=t1) & (t<=t2)] = a
         i+=2
     #noise = np.random.normal(mean,diff/100,len(t))
-    
-    #noise = np.random.normal(loc=mean,scale=sigma,size=len(t))
-   
+    #noise = np.random.normal(loc=mean,scale=sigma,size=len(t))  
     #noise = np.ones(len(t))
     #y += noise
     return y
@@ -58,32 +56,17 @@ def signalGeneration(amplitude=10, dA=5, period=4.0, df=0.5, dS=5, mean=0, sigma
     timestamps = np.arange(0, signalDuration, step)
     listOfEvents = events(L,signalDuration)
     
-    #apneaData = apnea(L, signalDuration, timestamps)
-    """
+    apneaData = apnea(L, signalDuration, timestamps)
     amplitude += vectorSimulation(L, dA, mean, sigma, signalDuration, timestamps,listOfEvents)
     s = vectorSimulation(L, dS, mean, sigma, signalDuration, timestamps,listOfEvents)
     freq += vectorSimulation(L, df, mean, sigma, signalDuration, timestamps,listOfEvents)
-    """
-    Ya = vectorSimulation(L, dA, mean, sigma, signalDuration, timestamps,listOfEvents)
-    amplitude += Ya
-    s = vectorSimulation(L, dS, mean, sigma, signalDuration, timestamps,listOfEvents)
-    Yf = vectorSimulation(L, df, mean, sigma, signalDuration, timestamps,listOfEvents)
-    freq += Yf
-    
-    plt.figure(figsize=(15,10))
-    plt.plot(timestamps,Ya)
-    plt.title("Amplitude")
-    
-    plt.figure(figsize=(15,10))
-    plt.plot(timestamps,Yf)
-    plt.title("Frequency")
     
     noise = np.random.normal(loc=mean,scale=sigma,size=len(timestamps))
     
     #signal = (amplitude / 2) * np.sin(2 * np.pi * freq * (timestamps % (1 / freq))) + s ## we talk about breathing amplitude in mm so its more the total amplitude than the half one, meaning it must be divided by two here
     signal = (amplitude / 2) * np.sin(2 * np.pi * freq * timestamps) + s + noise
     return timestamps * 1000, signal
-
+"""
 #parametres changeables
 amplitude = 10 #amplitude (mm)
 dA = 5 #variation d amplitude possible (mm)
@@ -102,7 +85,7 @@ plt.xlabel("Time [s]")
 plt.ylabel("Amplitude [mm]")
 plt.title("Breathing signal part 1")
 plt.xlim((0,100))
-"""
+
 plt.figure(figsize=(15,10))
 plt.plot(time,samples)
 plt.xlabel("Time [s]")
