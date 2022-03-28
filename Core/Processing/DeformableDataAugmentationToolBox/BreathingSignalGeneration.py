a8da7918cdcde0968af782a3fb8bba42730d9985
Gauthier Rotsart de Hertaing <grotsartdehe@maxwell.local>
2022-03-28 13:12:11 +0200

    breathin signal generation v1

--- Core/Processing/DeformableDataAugmentationToolBox/BreathingSignalGeneration.py
+++ Core/Processing/DeformableDataAugmentationToolBox/BreathingSignalGeneration.py
@@ -20,9 +20,10 @@ def events(L,Tend):
 
 #entre deux timestamps successifs, un event est créé
 #Un event correspond a une fonction echellon qui est ensuite bruitee
-def vectorSimulation(L,diff,mean,sigma,Tend,t):
-    timestamp = events(L,Tend)        
+def vectorSimulation(L,diff,mean,sigma,Tend,t,timestamp):
+    #timestamp = events(L,Tend)        
     y = np.zeros(len(t))
+    noise = np.zeros(len(t))
     i = 0
     while i < len(timestamp):
         if i+2 < len(timestamp):
@@ -31,9 +32,12 @@ def vectorSimulation(L,diff,mean,sigma,Tend,t):
             t2 = timestamp[i+2]
             y[(t>=t1) & (t<=t2)] = a
         i+=2
-    noise = np.random.normal(0,diff/100,len(t))
-    #noise = np.random.normal(mean,sigma,len(t))
-    y += noise
+    #noise = np.random.normal(mean,diff/100,len(t))
+    
+    #noise = np.random.normal(loc=mean,scale=sigma,size=len(t))
+   
+    #noise = np.ones(len(t))
+    #y += noise
     return y
 
 def apnea(L,Tend,t):
@@ -49,18 +53,37 @@ def apnea(L,Tend,t):
     return y
 
 #creation des donnees respiratoires
-def signalGeneration(amplitude=10, dA=5, period=4.0, df=0.5, dS=5, mean=0, sigma=0, step=0.2, signalDuration=100, L=2 / 30):
+def signalGeneration(amplitude=10, dA=5, period=4.0, df=0.5, dS=5, mean=0, sigma=1, step=0.2, signalDuration=100, L=2 / 30):
     freq = 1 / period
     timestamps = np.arange(0, signalDuration, step)
+    listOfEvents = events(L,signalDuration)
     
-    apneaData = apnea(L, signalDuration, timestamps)
-    amplitude += vectorSimulation(L, dA, mean, sigma, signalDuration, timestamps)
-    s = vectorSimulation(L, dS, mean, sigma, signalDuration, timestamps)
-    freq += vectorSimulation(L, df, mean, sigma, signalDuration, timestamps)
+    #apneaData = apnea(L, signalDuration, timestamps)
+    """
+    amplitude += vectorSimulation(L, dA, mean, sigma, signalDuration, timestamps,listOfEvents)
+    s = vectorSimulation(L, dS, mean, sigma, signalDuration, timestamps,listOfEvents)
+    freq += vectorSimulation(L, df, mean, sigma, signalDuration, timestamps,listOfEvents)
+    """
+    Ya = vectorSimulation(L, dA, mean, sigma, signalDuration, timestamps,listOfEvents)
+    amplitude += Ya
+    s = vectorSimulation(L, dS, mean, sigma, signalDuration, timestamps,listOfEvents)
+    Yf = vectorSimulation(L, df, mean, sigma, signalDuration, timestamps,listOfEvents)
+    freq += Yf
     
-    signal = (amplitude / 2) * np.sin(2 * np.pi * freq * (timestamps % (1 / freq))) + s ## we talk about breathing amplitude in mm so its more the total amplitude than the half one, meaning it must be divided by two here
+    plt.figure(figsize=(15,10))
+    plt.plot(timestamps,Ya)
+    plt.title("Amplitude")
+    
+    plt.figure(figsize=(15,10))
+    plt.plot(timestamps,Yf)
+    plt.title("Frequency")
+    
+    noise = np.random.normal(loc=mean,scale=sigma,size=len(timestamps))
+    
+    #signal = (amplitude / 2) * np.sin(2 * np.pi * freq * (timestamps % (1 / freq))) + s ## we talk about breathing amplitude in mm so its more the total amplitude than the half one, meaning it must be divided by two here
+    signal = (amplitude / 2) * np.sin(2 * np.pi * freq * timestamps) + s + noise
     return timestamps * 1000, signal
-"""
+
 #parametres changeables
 amplitude = 10 #amplitude (mm)
 dA = 5 #variation d amplitude possible (mm)
@@ -73,11 +96,18 @@ L = 2/30 #moyenne des evenements aleatoires
 
 time,samples = signalGeneration()
 time = np.arange(0, signalDuration, step)
-plt.figure()
+plt.figure(figsize=(15,10))
 plt.plot(time,samples)
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude [mm]")
-plt.title("Breathing signal")
-plt.xlim((0,50))
+plt.title("Breathing signal part 1")
+plt.xlim((0,100))
+"""
+plt.figure(figsize=(15,10))
+plt.plot(time,samples)
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [mm]")
+plt.title("Breathing signal part 2")
+plt.xlim((100,200))
 """
 
