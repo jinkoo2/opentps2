90833edda152daeff976c5fe5c8dead01af9237a
Sylvain Deffet <sylvaindeffet@gmail.com>
2022-04-08 11:42:27 +0200

    Button to reset secondary image

--- GUI/Viewer/DataViewerComponents/imageViewerActions.py
+++ GUI/Viewer/DataViewerComponents/imageViewerActions.py
@@ -1,12 +1,12 @@
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QAction, QComboBox, QLabel, QWidgetAction
+from PyQt5.QtWidgets import QAction, QComboBox, QLabel, QWidgetAction, QPushButton
 
 from GUI.Viewer.DataViewerComponents.dataViewerToolbar import DataViewerToolbar
 from GUI.Viewer.DataViewerComponents.imageViewer import ImageViewer
 
 
 class ImageViewerActions:
-    def __init__(self, imageViewer: ImageViewer):
+    def __init__(self, imageViewer:ImageViewer):
         self._imageViewer = imageViewer
 
         self._viewTypeToStr = {self._imageViewer.viewerTypes.AXIAL: 'Axial',
@@ -23,13 +23,19 @@ class ImageViewerActions:
         self._viewTypeAction = QWidgetAction(None)
         self._viewTypeAction.setDefaultWidget(self._viewTypeCombo)
 
+
+        self._resetSecondaryImgButton = QPushButton('Reset secondary img')
+        self._resetSecondaryImgButton.setFixedHeight(16)
+        self._resetSecondaryImgButton.clicked.connect(self._resetSecondaryImg)
+        self._resetSecondaryImgAction = QWidgetAction(None)
+        self._resetSecondaryImgAction.setDefaultWidget(self._resetSecondaryImgButton)
+
         self.hide()
 
         self._viewTypeCombo.setCurrentIndex(self._viewTypeToIndex(self._imageViewer.viewType))
-
         self._viewTypeCombo.currentIndexChanged.connect(self._handleViewTypeSelection)
-
         self._imageViewer.viewTypeChangedSignal.connect(self._handleExternalViewTypeChange)
+
 
     def _viewTypeToIndex(self, viewType):
         return list(self._viewTypeToStr.keys()).index(viewType)
@@ -40,15 +46,18 @@ class ImageViewerActions:
     def addToToolbar(self, toolbar:DataViewerToolbar):
         self._separator = toolbar.addSeparator()
         toolbar.addAction(self._viewTypeAction)
+        toolbar.addAction(self._resetSecondaryImgAction)
 
     def hide(self):
         if not self._separator is None:
             self._separator.setVisible(False)
         self._viewTypeAction.setVisible(False)
+        self._resetSecondaryImgAction.setVisible(False)
 
     def show(self):
         self._separator.setVisible(True)
         self._viewTypeAction.setVisible(True)
+        self._resetSecondaryImgAction.setVisible(True)
 
     def _handleViewTypeSelection(self, selectionIndex):
         selectionText = self._viewTypeCombo.itemText(selectionIndex)
@@ -56,3 +65,6 @@ class ImageViewerActions:
 
     def _handleExternalViewTypeChange(self, viewType):
         self._viewTypeCombo.setCurrentIndex(self._viewTypeToIndex(viewType))
+
+    def _resetSecondaryImg(self):
+        self._imageViewer.secondaryImage = None
