19cad045092f47d14eada3feab6fde6abed1cea7
Sylvain Deffet <sylvaindeffet@gmail.com>
2022-05-04 15:35:09 +0200

    Reload python modules

--- GUI/Viewer/viewerPanel.py
+++ GUI/Viewer/viewerPanel.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
 from GUI.Viewer.dataViewer import DroppedObject
 from GUI.Viewer.gridFourElements import GridFourElements
+import GUI.Viewer.viewerToolbar
 from GUI.Viewer.viewerToolbar import ViewerToolbar
 
 class ViewerPanel(QWidget):
