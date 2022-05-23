2218176d12c54c7a5b96684ec5b3ee7a9b24f8b8
Sylvain Deffet <sylvaindeffet@gmail.com>
2022-05-23 17:27:20 +0200

    Framework for extensions

--- GUI/Panels/mainToolbar.py
+++ GUI/Panels/mainToolbar.py
@@ -1,9 +1,11 @@
 import functools
+import glob
+import logging
+import os
 
 from PyQt5.QtWidgets import QToolBox, QWidget
 
 from Core.event import Event
-from Extensions.FLASH.flashPanel import FlashPanel
 from GUI.Panels.doseComputationPanel import DoseComputationPanel
 from GUI.Panels.patientDataPanel import PatientDataPanel
 from GUI.Panels.planOptiPanel import PlanOptiPanel
@@ -12,6 +14,10 @@ from GUI.Panels.scriptingPanel.scriptingPanel import ScriptingPanel
 from GUI.Panels.breathingSignalPanel import BreathingSignalPanel
 from GUI.Panels.drrPanel import DRRPanel
 
+import Extensions as extensionModule
+
+
+logger = logging.getLogger(__name__)
 
 
 class MainToolbar(QToolBox):
@@ -69,11 +75,7 @@ class MainToolbar(QToolBox):
         item = self.ToolbarItem(xRayProjPanel, 'DRR')
         self.showItem(item)
 
-        # Extensions
-        flashPanel = FlashPanel(self._viewController)
-        item = self.ToolbarItem(flashPanel, 'FLASH')
-        self.showItem(item)
-
+        self._addExtenstions()
 
         self._addVisibilityListenerToAllItems()
 
@@ -101,7 +103,41 @@ class MainToolbar(QToolBox):
         self.removeItem(self._items.index(item))
         self._items.remove(item)
 
-
     @property
     def items(self):
         return [item for item in self._items]
+
+    def _addExtenstions(self):
+        extensionFilesFromDir = lambda d:[f for f in glob.glob(os.path.join(d, "*.py")) if "extension" in f or "Extension"]
+        extensionFiles = extensionFilesFromDir(extensionModule.__path__[0])
+        subdirs = glob.glob(os.path.join(extensionModule.__path__[0], '*/'), recursive=False)
+        for subdir in subdirs:
+            extensionFiles.extend(extensionFilesFromDir(subdir))
+
+        for extensionFile in extensionFiles:
+            try:
+                p = None
+
+                extensionName = os.path.splitext(os.path.basename(extensionFile))[0]
+
+                strToEval = 'from Extensions.'
+                extensionsFound = False
+                for dirElem in extensionFile.split(os.path.sep):
+                    if extensionsFound:
+                        if os.path.splitext(dirElem)[0]==extensionName:
+                            break
+                        else:
+                            strToEval += dirElem + '.'
+                    else:
+                        if dirElem=='Extensions':
+                            extensionsFound = True
+
+
+                strToEval +=  extensionName + ' import Panel\n'
+                strToEval += 'p = Panel(self._viewController)\n'
+                strToEval += 'item = self.ToolbarItem(p, \'' + extensionName + '\')\n'
+                strToEval += 'self.showItem(item)'
+                exec(strToEval)
+            except Exception as e:
+                logger.error(str(e))
+
