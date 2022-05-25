59df96e4a2eb8ae525d52064a581c4d02bd6779d
Gauthier Rotsart de Hertaing <grotsartdehe@maxwell.local>
2022-05-25 15:29:09 +0200

    new version of breathing signals

--- Core/Data/DynamicData/breathingSignals.py
+++ Core/Data/DynamicData/breathingSignals.py
@@ -19,33 +19,30 @@ class BreathingSignal(PatientData):
 
 # synthetic breathing data
 class SyntheticBreathingSignal(BreathingSignal):
-    def __init__(self, amplitude=10, variationAmplitude=5, breathingPeriod=4, variationFrequency=0.5, shift=5, meanNoise=0,
-                 varianceNoise=0.01, samplingPeriod=0.2, simulationTime=100, meanEvent=2 / 30, patientInfo=None, name="Breathing Signal"):
+    def __init__(self, amplitude=10, breathingPeriod=4, meanNoise=0,
+                 varianceNoise=1, samplingPeriod=0.2, simulationTime=100, coeffMin = 0.10, coeffMax = 0.15, meanEvent = 1/60, meanEventApnea=0/120, patientInfo=None, name="Breathing Signal"):
         super().__init__(patientInfo=patientInfo, name=name)
 
         self.amplitude = amplitude  # amplitude (mm)
-        self.variationAmplitude = variationAmplitude  # variation d amplitude possible (mm)
         self.breathingPeriod = breathingPeriod  # periode respiratoire (s)
-        self.variationFrequency = variationFrequency  # variation de frequence possible (Hz)
-        self.shift = shift  # shift du signal (mm)
         self.meanNoise = meanNoise
         self.varianceNoise = varianceNoise
         self.samplingPeriod = samplingPeriod  # periode d echantillonnage
         self.simulationTime = simulationTime  # temps de simulation
-        self.meanEvent = meanEvent  # moyenne des evenements aleatoires
+        self.coeffMin = coeffMin #coefficient minimal pour le changement d amplitude
+        self.coeffMax = coeffMax #coefficient maximal pour le changement d amplitude
+        self.meanEvent = meanEvent #nombre moyen d evenements
+        self.meanEventApnea = meanEventApnea #nombre moyen d apnees
 
 
     def generate1DBreathingSignal(self):
-        self.timestamps, self.breathingSignal = signalGeneration(self.amplitude, self.variationAmplitude, self.breathingPeriod,
-                                                                 self.variationFrequency, self.shift, self.meanNoise, self.varianceNoise,
-                                                                 self.samplingPeriod, self.simulationTime, self.meanEvent)
+        self.timestamps, self.breathingSignal = signalGeneration(self.amplitude, self.breathingPeriod, self.meanNoise, self.varianceNoise,
+                                                                 self.samplingPeriod, self.simulationTime, self.coeffMin, self.coeffMax, 
+                                                                 self.meanEvent, self.meanEventApnea)
         return self.breathingSignal
 
     def generate3DBreathingSignal(self):
-        self.timestamps, self.breathingSignal = signal3DGeneration(self.amplitude, self.variationAmplitude,
-                                                                 self.breathingPeriod,
-                                                                 self.variationFrequency, self.shift, self.meanNoise,
-                                                                 self.varianceNoise,
-                                                                 self.samplingPeriod, self.simulationTime,
-                                                                 self.meanEvent)
+        self.timestamps, self.breathingSignal = signal3DGeneration(self.amplitude,self.breathingPeriod, self.meanNoise, self.varianceNoise,
+                                                                   self.samplingPeriod, self.simulationTime, self.coeffMin, self.coeffMax,
+                                                                   self.meanEvent, self.meanEventApnea)
         return self.breathingSignal
