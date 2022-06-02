deeafeabf2a8b34afed2fcdd6d851382d9a943cc
Damien Dasnoy <damien.dasnoy@gmail.com>
2022-06-02 10:05:10 +0200

    first version of an organ shrinking method applied on a model, returning the new model, the shrinked organ new mask and new center of mass

--- Core/Processing/DeformableDataAugmentationToolBox/shrinkOrganTest.py
+++ Core/Processing/DeformableDataAugmentationToolBox/shrinkOrganTest.py
@@ -9,25 +9,22 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.ndimage import gaussian_filter
 
 
-def shrinkOrgan(image, organContour, shrinkSize = [2, 2, 2]):
+def shrinkOrgan(model, organMask, shrinkSize = [2, 2, 2]):
 
     ## get organ COM
-    gtvCenterOfMass = organContour.getCenterOfMass(image.origin, image.gridSize, image.spacing)
-    gtvCenterOfMassInVoxels = getVoxelIndexFromPosition(gtvCenterOfMass, image)
-    print('Used ROI name', organContour.name)
-    print('Used ROI center of mass :', gtvCenterOfMass)
-    print('Used ROI center of mass in voxels:', gtvCenterOfMassInVoxels)
-
-    GTVMask = organContour.getBinaryMask(origin=image.origin, gridSize=image.gridSize, spacing=image.spacing)
-
+    organCOM = organMask.centerOfMass
+    organCOMInVoxels = getVoxelIndexFromPosition(organCOM, model.midp)
+    # print('Used ROI name', organMask.name)
+    # print('Used ROI center of mass :', organCOM)
+    # print('Used ROI center of mass in voxels:', organCOMInVoxels)
     # plt.figure()
-    # plt.imshow(image.imageArray[:, :, gtvCenterOfMassInVoxels[2]])
-    # plt.imshow(GTVMask.imageArray[:, :, gtvCenterOfMassInVoxels[2]], alpha=0.5)
+    # plt.imshow(model.midp.imageArray[:, :, organCOMInVoxels[2]])
+    # plt.imshow(organMask.imageArray[:, :, organCOMInVoxels[2]], alpha=0.5)
     # plt.show()
 
     ## get the shrink size in voxels
     print('Shrink size in mm:', shrinkSize)
-    shrinkSizeInVoxels = np.round(shrinkSize / image.spacing).astype(np.uint8)
+    shrinkSizeInVoxels = np.round(shrinkSize / model.midp.spacing).astype(np.uint8)
     print('Shrink size in voxels:', shrinkSizeInVoxels)
 
     # get the structural element used for the erosion and dilation
@@ -41,94 +38,92 @@ def shrinkOrgan(image, organContour, shrinkSize = [2, 2, 2]):
     # plt.show()
 
     ## apply an erosion and dilation using Cupy
-    cupyGTVMask = cupy.asarray(GTVMask.imageArray)
-    erodedGTVMask = cupy.asnumpy(cupyx.scipy.ndimage.binary_erosion(cupyGTVMask, structure=cupy.asarray(structuralElementXYZ)))
-    dilatedGTVMask = cupy.asnumpy(cupyx.scipy.ndimage.binary_dilation(cupyGTVMask, structure=cupy.asarray(structuralElementXYZ)))
+    cupyOrganMask = cupy.asarray(organMask.imageArray)
+    erodedOrganMask = cupy.asnumpy(cupyx.scipy.ndimage.binary_erosion(cupyOrganMask, structure=cupy.asarray(structuralElementXYZ)))
+    dilatedOrganMask = cupy.asnumpy(cupyx.scipy.ndimage.binary_dilation(cupyOrganMask, structure=cupy.asarray(structuralElementXYZ)))
 
-    erodedBand = GTVMask.imageArray ^ erodedGTVMask
-    dilatedBand = dilatedGTVMask ^ GTVMask.imageArray
+    ## get the new COM after mask erosion
+    organROIMaskCopy = copy.deepcopy(organMask)
+    organROIMaskCopy.imageArray = erodedOrganMask
+    erodedMaskCOM = organROIMaskCopy.centerOfMass
+
+    erodedBand = organMask.imageArray ^ erodedOrganMask
+    dilatedBand = dilatedOrganMask ^ organMask.imageArray
 
     # plt.figure()
     # plt.subplot(1, 2, 1)
-    # plt.imshow(erodedBand[:, :, gtvCenterOfMassInVoxels[2]])
+    # plt.imshow(erodedBand[:, :, organCOMInVoxels[2]])
     # plt.subplot(1, 2, 2)
-    # plt.imshow(dilatedBand[:, :, gtvCenterOfMassInVoxels[2]])
+    # plt.imshow(dilatedBand[:, :, organCOMInVoxels[2]])
     # plt.show()
 
     erodedBandPoints = np.argwhere(erodedBand == 1)
     dilatedBandPoints = np.argwhere(dilatedBand == 1)
 
-    newImg = copy.deepcopy(image)
+    newArray = copy.deepcopy(model.midp.imageArray)
+
+    print('Start filling the eroded band with new values, this might take a few minutes')
 
     for pointIndex, point in enumerate(erodedBandPoints):
 
-
-        # print(dilatedBandPoints - point)
         distances = np.sqrt(np.sum(np.square(dilatedBandPoints - point), axis=1))
         distances = np.expand_dims(distances, axis=1)
-        # print(distances.shape)
+
+        ##
         dilBandPointsAndDists = np.concatenate((dilatedBandPoints, distances), axis=1)
 
-
-        # print(dilBandPointsAndDists.shape)
-        # print(dilBandPointsAndDists[:10])
-        # sortedPointAndDists = np.sort(dilBandPointsAndDists, axis=)
+        ##
         sortedPointAndDists = dilBandPointsAndDists[dilBandPointsAndDists[:, 3].argsort()]
 
         ## take closest 10% of points
         sortedPointAndDists = sortedPointAndDists[:int((10 / 100) * dilBandPointsAndDists.shape[0])]
-        # print('-----------')
-        # print(sortedPointAndDists[:5])
-        # print(sortedPointAndDists.shape)
 
-        imageValuesToUse = image.imageArray[sortedPointAndDists[:, :3].astype(np.uint8)]
 
-        # print(imageValuesToUse)
+        imageValuesToUse = model.midp.imageArray[sortedPointAndDists[:, :3].astype(np.uint8)]
+
         meanValueOfClosestPoints = np.mean(imageValuesToUse)
-        varValueOfClosestPoints = np.std(imageValuesToUse)
+        # varValueOfClosestPoints = np.std(imageValuesToUse)
 
+        ## this is not ideal, hard coded value which might not work for other organs than lung
         meanValueOfClosestPoints -= 280
+
         newValue = np.random.normal(meanValueOfClosestPoints, 70)
-        newImg.imageArray[point[0], point[1], point[2]] = newValue
+        newArray[point[0], point[1], point[2]] = newValue
 
-        print('Point', pointIndex, point, 'meanValueOfClosestPointsAdjusted:', meanValueOfClosestPoints, 'new value:', newValue)
+        # print('Point', pointIndex, point, 'meanValueOfClosestPointsAdjusted:', meanValueOfClosestPoints, 'new value:', newValue)
 
 
-    cupyNewImg = cupy.asarray(newImg.imageArray)
+    cupyNewImg = cupy.asarray(newArray)
     smoothedImg = cupy.asnumpy(cupyx.scipy.ndimage.gaussian_filter(cupyNewImg, 1))
 
-    # smoothedImg = gaussian_filter(newImg.imageArray[GTVMask.imageArray], 0.5)
-    newImg.imageArray[dilatedGTVMask] = smoothedImg[dilatedGTVMask]
-
+    newModel = copy.deepcopy(model)
+    newModel.midp.imageArray[dilatedOrganMask] = smoothedImg[dilatedOrganMask]
+    newModel.midp.name = 'MidP_ShrinkedGTV'
 
     # erodedBandMean = np.mean(image.imageArray[erodedBand == 1])
     # dilatedBandMean = np.mean(image.imageArray[dilatedBand == 1])
     # print(erodedBandMean, dilatedBandMean)
 
-    plt.figure()
-    plt.subplot(2, 2, 1)
-    # plt.imshow(image.imageArray[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    plt.imshow(smoothedImg[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    plt.subplot(2, 2, 2)
-    plt.imshow(smoothedImg2[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    plt.subplot(2, 2, 3)
-    plt.imshow(newImg.imageArray[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    plt.subplot(2, 2, 4)
-    plt.imshow(newImg2.imageArray[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    # plt.imshow(image.imageArray[100:200, 100:200, gtvCenterOfMassInVoxels[2]]-newImg.imageArray[100:200, 100:200, gtvCenterOfMassInVoxels[2]])
-    plt.show()
+    # fig, axs = plt.subplots(1, 5, constrained_layout=True)
+    # fig.suptitle('organ shrinking example', fontsize=16)
+    # axs[0].imshow(model.midp.imageArray[:, :, organCOMInVoxels[2]])
+    # axs[0].set_title('original image')
+    #
+    # axs[1].imshow(newArray[:, :, organCOMInVoxels[2]])
+    # axs[1].set_title('values replaced image')
+    #
+    # axs[2].imshow(smoothedImg[:, :, organCOMInVoxels[2]])
+    # axs[2].set_title('smoothed image')
+    #
+    # axs[3].imshow(newModel.midp.imageArray[:, :, organCOMInVoxels[2]])
+    # axs[3].set_title('result image')
+    #
+    # axs[4].imshow(model.midp.imageArray[:, :, organCOMInVoxels[2]] - newModel.midp.imageArray[:, :, organCOMInVoxels[2]])
+    # axs[4].set_title('original-shrinked diff')
+    #
+    # plt.show()
 
-    fig, axs = plt.subplots(2, 1, constrained_layout=True)
-    fig.suptitle('organ shrinking example', fontsize=16)
-    axs[0].imshow(image.imageArray[:, :, gtvCenterOfMassInVoxels[2]])
-    axs[0].set_title('original image')
-
-    axs[1].imshow(image.imageArray[:, :, gtvCenterOfMassInVoxels[2]])
-    axs[1].set_title('original image')
-
-    plt.show()
-
-    return newImg
+    return newModel, erodedOrganMask, erodedMaskCOM
 
 def affineTransformCupy():
 
