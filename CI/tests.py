2c0b17489da2616f5ce32b9d4c35f68e99edc7d2
Sylvain Deffet <sylvaindeffet@gmail.com>
2022-09-28 13:57:23 +0200

    Update test

--- CI/tests.py
+++ CI/tests.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+from opentps.core.data import PatientList
 from opentps.core.data.images import CTImage
 from opentps.core.data.images import ROIMask
 from opentps.core.data import Patient
