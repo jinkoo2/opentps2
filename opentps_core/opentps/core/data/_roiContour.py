73646d3518a4d15bc845128e2360310b61b58067
Sylvain Deffet <sylvaindeffet@gmail.com>
2022-10-13 18:01:19 +0200

    Fix binary mask estimated on a grid way too large

--- opentps_core/opentps/core/data/_roiContour.py
+++ opentps_core/opentps/core/data/_roiContour.py
@@ -67,9 +67,9 @@ class ROIContour(PatientData):
         zDiff = np.abs(np.diff(allZ))
         zDiff[zDiff == 0] = np.Inf
 
-        contourSpacing[0] = np.max((xDiff.min(), minSpatialResolution))
-        contourSpacing[1] = np.max((yDiff.min(), minSpatialResolution))
-        contourSpacing[2] = np.max((zDiff.min(), minSpatialResolution))
+        contourSpacing[0] = np.min((xDiff.min(), minSpatialResolution))
+        contourSpacing[1] = np.min((yDiff.min(), minSpatialResolution))
+        contourSpacing[2] = np.min((zDiff.min(), minSpatialResolution))
 
         contourGridSize[0] = int(round((allX[-1]-contourOrigin[0])/contourSpacing[0])) + 1
         contourGridSize[1] = int(round((allY[-1]-contourOrigin[1])/contourSpacing[1])) + 1
