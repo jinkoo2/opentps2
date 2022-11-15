20df273ea67084a2b103649d72642f3b562663a8
sowuy <sophie.wuyckens@uclouvain.be>
2022-11-15 10:55:34 +0100

    create ROIMask rings

--- opentps_core/opentps/core/data/images/_roiMask.py
+++ opentps_core/opentps/core/data/images/_roiMask.py
@@ -102,6 +102,27 @@ class ROIMask(Image3D):
     def _dilateScipy(self, filt):
         self._imageArray = morphology.binary_dilation(self._imageArray, structure=filt)
 
+    def createMaskRings(self, nRings, radius):
+        """
+            Create a ring ROI to obtain nice gradient dose around the ROI
+            nRings: Number of rings to be created
+            radius: thickness of each ring in mm
+        """
+        rings = []
+        targetSizes = [self]
+        maskCopy = self.copy()
+
+        for i in range(nRings):
+            maskCopy.dilate(radius)
+            targetSizes.append(maskCopy.copy())
+
+        for i in range(nRings):
+            ringMask = self.copy()
+            ringMask.imageArray = np.logical_xor(targetSizes[i + 1].imageArray, targetSizes[i].imageArray)
+            ringMask.name = 'ring_' + str(i + 1)
+            rings.append(ringMask)
+        return rings
+
     def erode(self, radius=1.0, filt=None, tryGPU=True):
         if filt is None:
             radius = radius / np.array(self.spacing)
