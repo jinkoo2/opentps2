250c2ef0d138b6fc7df3a50e50a45f0b6dd91403
vhamaide <valentin.hamaide@iba-group.com>
2022-12-12 15:44:15 +0100

    Simple delivery timing model without scanAlgo + code refactoring

--- opentps_core/opentps/core/examples/doseDeliverySimulation/PBSDeliveryTimings.py
+++ opentps_core/opentps/core/examples/doseDeliverySimulation/PBSDeliveryTimings.py
@@ -1,24 +1,27 @@
-import sys
-sys.path.append('..')
-
+import numpy as np
+np.random.seed(42)
+from opentps.core.data.plan._planIonBeam import PlanIonBeam
+from opentps.core.data.plan._planIonLayer import PlanIonLayer
 from opentps.core.data.plan._rtPlan import RTPlan
-from opentps.core.processing.planDeliverySimulation.scanAlgoDeliveryTimings import BDT
+from opentps.core.processing.planDeliverySimulation.scanAlgoBeamDeliveryTimings import ScanAlgoBeamDeliveryTimings
+from opentps.core.processing.planDeliverySimulation.simpleBeamDeliveryTimings import SimpleBeamDeliveryTimings
 from opentps.core.io.dicomIO import readDicomPlan
 
-"""
-#### Config file: text file openTPS_workspace/config/DeliverySimulationConfig with 2 lines
-that need to be filled with ScanAlgo parameters:
-gantry = <TYPE OF GANTRY>
-gateway = <URL>
-"""
+# Create random plan
+plan = RTPlan()
+plan.appendBeam(PlanIonBeam())
+energies = np.array([130, 140, 150, 160, 170])
+for m in energies:
+    layer = PlanIonLayer(m)
+    x = 10*np.random.random(5) - 5
+    y = 10*np.random.random(5) - 5
+    mu = 5*np.random.random(5)
 
-# load OpenTPS plan
-plan_path = r"C:\Users\valentin.hamaide\data\ARIES\patient_0\plan_4D_robust.dcm"
-plan = readDicomPlan(plan_path)
+    layer.appendSpot(x, y, mu)
+    plan.beams[0].appendLayer(layer)
 
-#### Conventional PT usage.Gantry
-# Add PBS timings into a plan
-bdt = BDT(plan)
+
+bdt = SimpleBeamDeliveryTimings(plan)
 plan_with_timings = bdt.getPBSTimings(sort_spots="true")
 
 # print plan
