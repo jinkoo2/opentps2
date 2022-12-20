5b8d02b827fab794676ad2697c0c115447daf16c
Gauthier Rotsart <gauthier.rotsart@uclouvain.be>
2022-12-20 09:32:00 +0100

    evaluation of the shrink

--- opentps_core/opentps/core/processing/dataComparison/dynamic3DModelComparison.py
+++ opentps_core/opentps/core/processing/dataComparison/dynamic3DModelComparison.py
@@ -1,9 +1,11 @@
 import math as m 
+import numpy as np
 
-from opentps.core.processing.imageProcessing.resampler3D import resample
+from opentps.core.processing.imageProcessing.resampler3D import resample, resampleOnImage3D
 from opentps.core.processing.registration.registrationRigid import RegistrationRigid
 from opentps.core.processing.dataComparison.image3DComparison import getTranslationAndRotation
 from opentps.core.processing.dataComparison.contourComparison import getBaselineShift
+from opentps.core.processing.dataComparison.testShrink import eval
 
 def compareModels(model1, model2, targetContourToUse1, targetContourToUse2):
     dynMod1 = model1.getPatientDataOfType("Dynamic3DModel")[0]
@@ -42,6 +44,31 @@ def compareModels(model1, model2, targetContourToUse1, targetContourToUse2):
     gtvContour2 = rtStruct2.getContourByName(targetContourToUse2)
     GTVMask2 = gtvContour2.getBinaryMask(origin=dynMod2.midp.origin, gridSize=dynMod2.midp.gridSize,
                                         spacing=dynMod2.midp.spacing)
+    deformedMask2 = transform.deformImage(GTVMask2)
+    print(GTVMask1.imageArray.shape, deformedMask2.imageArray.shape)
+    cm1 = GTVMask1.centerOfMass
+    cm2 = deformedMask2.centerOfMass
+    print("baseline shift", cm2 - cm1)
+    deformedMask2 = resampleOnImage3D(data=deformedMask2, fixedImage=GTVMask1)
+    #GTVMask2 = resampleImage3D(GTVMask2, spacing=[1, 1, 1])
+    print(GTVMask1.imageArray.shape, deformedMask2.imageArray.shape)
+    diff = deformedMask2.imageArray ^ GTVMask1.imageArray
+    ligne = eval(deformedMask2.imageArray, diff)
 
+    print("before transpose", deformedMask2.imageArray.shape, diff.shape)
+    GTVMask2_t = deformedMask2.imageArray.transpose(0, 2, 1)
+    diff_t = diff.transpose(0, 2, 1)
+    print("after transpose 1", GTVMask2_t.shape, diff_t.shape)
+    col = eval(GTVMask2_t, diff_t)
+
+    GTVMask2_tt = GTVMask2_t.transpose(1,0,2)
+    diff_tt = diff_t.transpose(1,0,2)
+    print("after transpose 2", GTVMask2_tt.shape, diff_tt.shape)
+    col2 = eval(GTVMask2_tt, diff_tt)
+
+    print(np.mean(ligne), np.mean(col), np.mean(col2))
+    print("in mm", np.mean(ligne)*GTVMask2.spacing[0], np.mean(col)*GTVMask2.spacing[1], np.mean(col2)*GTVMask2.spacing[2])
     baselineShift = getBaselineShift(fixedMask=GTVMask1, movingMask=GTVMask2, transform=transform)
     print("Baseline shift: ", baselineShift)
+    print(len(ligne))
+    print(ligne)
