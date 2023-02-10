c3b1d909dff90bcbf00f3666c3c0577dc87dd617
Gauthier Rotsart <gauthier.rotsart@uclouvain.be>
2023-02-10 11:50:23 +0100

    change patient id

--- opentps_core/opentps/core/processing/dataComparison/comparisonTest.py
+++ opentps_core/opentps/core/processing/dataComparison/comparisonTest.py
@@ -3,14 +3,14 @@ from opentps.core.io.serializedObjectIO import loadDataStructure
 
 organ = 'lung'
 study = 'FDGorFAZA_study'
-patientFolder = 'Patient_13'#'Patient_12'
+patientFolder = 'Patient_12'#'Patient_12'
 basePath = '/DATA2/public/'#'/data/WalBan/'
 dataName = 'dynModAndROIs.p'
 #pour le patient 12, il faut changer l'ordre chronologique car le masque au temps 1 est plus petit qu'au temps 2
 patientComplement1 = '/2/FDG2'
 patientComplement2 = '/1/FDG1'
 targetContourToUse1 = 'GTVp'
-targetContourToUse2 = 'GTVp'#'gtv t'
+targetContourToUse2 = 'gtv t'#'GTVp'
 
 dataPath1 = basePath + organ + '/' + study + '/' + patientFolder + patientComplement1 + '/' + dataName
 dataPath2 = basePath + organ + '/' + study + '/' + patientFolder + patientComplement2 + '/' + dataName
