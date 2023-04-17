2ccb59dbc23f3d193dff500ad7f75931f34014c5
Sylvain Deffet <sylvaindeffet@gmail.com>
2023-04-17 10:38:03 +0200

    Remove deepCopyWithoutEvents which seems not used

--- opentps_core/opentps/core/data/_patientData.py
+++ opentps_core/opentps/core/data/_patientData.py
@@ -48,40 +48,6 @@ class PatientData:
         #result.patient = patient
         return result
 
-    def deepCopyWithoutEventExceptNdArray(self):
-        self._staticVars["deepCopyingExceptNdArray"] = True
-        try:
-            newObj = copy.deepcopy(self)
-        except Exception as e:
-            self._staticVars["deepCopyingExceptNdArray"] = False
-            raise(e)
-        self._staticVars["deepCopyingExceptNdArray"] = False
-
-        return newObj._recuresivelyResetEvents()
-
-    def deepCopyWithoutEvent(self):
-        newObj = copy.deepcopy(self)
-        return newObj._recuresivelyResetEvents()
-
-    def _recuresivelyResetEvents(self, checkedItems = []):
-        # Loop on all attributes and remove Events
-        for attrKey, attrVal in self.__dict__.items():
-            try:
-                if isinstance(attrVal, Event):
-                    self.__dict__[attrKey] = Event(attrVal.objectType)
-                elif not (attrVal in checkedItems) :
-                    print('Deep copying ' + attrVal)
-                    checkedItems.append(attrVal) # Avoid infinite loop
-                    self.__dict__[attrKey] = attrVal._recuresivelyResetEvents(checkedItems=checkedItems)
-                else:
-                    print('Skipping ' + attrVal)
-                    pass
-            except:
-                # newObj.__dict__[attrKey] is a base type instance not an object
-                pass
-
-        return self
-
     @property
     def name(self) -> str:
         return self._name
@@ -137,31 +103,15 @@ class EventTestCase(unittest.TestCase):
             self.eventField.connect(EventTestCase.dummyMethod)
             self.objectField.eventField.connect(EventTestCase.dummyMethod)
 
-    def dummyMethod(self):
-        from PyQt5.QtWidgets import QWidget
-        QWidget()
-        return
-
 
     def testDeepCopyWithoutEvent(self):
         obj = self.TestObj()
 
         newObj = obj.deepCopyWithoutEvent()
+        print('I', newObj.eventField)
+        print('I', newObj.objectField.eventField)
         self.assertIsNone(newObj.eventField)
         self.assertIsNone(newObj.objectField.eventField)
         self.assertIsNone(newObj.selfField.eventField)
         self.assertEqual(newObj.stringField, obj.stringField)
         self.assertEqual(newObj.selfField.stringField, obj.stringField)
-
-    def testShallowCopyWithoutEvent(self):
-        obj = self.TestObj()
-
-        newObj = obj.shallowCopyWithoutEvent()
-        self.assertIsNone(newObj.eventField)
-        self.assertIsNone(newObj.objectField.eventField)
-        self.assertIsNone(newObj.selfField.eventField)
-        self.assertEqual(newObj.stringField, obj.stringField)
-        self.assertEqual(newObj.selfField.stringField, obj.stringField)
-        self.assertEqual(obj, newObj.selfField)
-        self.assertEqual(obj, newObj.objectField.parent)
-
